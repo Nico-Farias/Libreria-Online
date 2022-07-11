e4daf922e6a2d372ca920cb5686c955ab19c30c2
Nico-Farias <nicoscs12@gmail.com>
2022-07-11 10:49:37 -0300

    img

--- js/libros.data.js
+++ js/libros.data.js
@@ -3,7 +3,7 @@ const libros = [
         "id": "1",
         "title": "Caos Magali Tajes",
         "categoria": "poesia",
-        "image": "./img/Caos.jpg",
+        "image": "../img/Caos.jpg",
         "price": 2999,
         "stock": 7,
         "cantidad": 1
@@ -12,7 +12,7 @@ const libros = [
         "id": "2",
         "title": "Culpa mia Mercedes Ron",
         "categoria": "ficcion",
-        "image": "./img/culpaMia.jpg",
+        "image": "../img/culpaMia.jpg",
         "price": 3233,
         "stock": 7,
         "cantidad": 1
@@ -21,7 +21,7 @@ const libros = [
         "id": "3",
         "title": "El dia que dejo de helar en alazca",
         "categoria": "ficcion",
-        "image": "./img/elDiaQueDejo.png",
+        "image": "../img/elDiaQueDejo.png",
         "price": 3799,
         "stock": 7,
         "cantidad": 1
@@ -30,7 +30,7 @@ const libros = [
         "id": "4",
         "title": "Ciudades de ceniza trilogia",
         "categoria": "juvenil",
-        "image": "./img/trilogiafuero.jpg",
+        "image": "../img/trilogiafuero.jpg",
         "price": 2420,
         "stock": 7,
         "cantidad": 1
@@ -39,7 +39,7 @@ const libros = [
         "id": "5",
         "title": "Furia tracy wolff",
         "categoria": "ficcion",
-        "image": "./img/furia.jpg",
+        "image": "../img/furia.jpg",
         "price": 3410,
         "stock": 7,
         "cantidad": 1
@@ -48,7 +48,7 @@ const libros = [
         "id": "6",
         "title": "Despues del boulevart",
         "categoria": "juvenil",
-        "image": "./img/Despues-de-el-Boulevard-2--i6n21104560.jpg",
+        "image": "../img/Despues-de-el-Boulevard-2--i6n21104560.jpg",
         "price": 2899,
         "stock": 7,
         "cantidad": 1
@@ -57,7 +57,7 @@ const libros = [
         "id": "7",
         "title": "Arde la vida Magali Tajes",
         "categoria": "poesia",
-        "image": "./img/ardeLaVida.jpg",
+        "image": "../img/ardeLaVida.jpg",
         "price": 2699,
         "stock": 7,
         "cantidad": 1
@@ -66,7 +66,7 @@ const libros = [
         "id": "8",
         "title": "Culpa tuya Mercedes Ron",
         "categoria": "ficcion",
-        "image": "./img/culpaTUya.jpg",
+        "image": "../img/culpaTUya.jpg",
         "price": 3533,
         "stock": 7,
         "cantidad": 1
@@ -75,7 +75,7 @@ const libros = [
         "id": "9",
         "title": "Nosotros en la luna Alice Kellen",
         "categoria": "ficcion",
-        "image": "./img/nosotrosEnLaLona.jpg",
+        "image": "../img/nosotrosEnLaLona.jpg",
         "price": 1799,
         "stock": 7,
         "cantidad": 1
@@ -84,7 +84,7 @@ const libros = [
         "id": "10",
         "title": "Malibu renace",
         "categoria": "ficcion",
-        "image": "./img/maliburenaca.jpg",
+        "image": "../img/maliburenaca.jpg",
         "price": 2120,
         "stock": 7,
         "cantidad": 1
@@ -93,7 +93,7 @@ const libros = [
         "id": "11",
         "title": "Los siete maridos de Evelyn",
         "categoria": "ficcion",
-        "image": "./img/lossieteMaridos.jpg",
+        "image": "../img/lossieteMaridos.jpg",
         "price": 3110,
         "stock": 7,
         "cantidad": 1
@@ -102,7 +102,7 @@ const libros = [
         "id": "12",
         "title": "Perfectos mentirosos",
         "categoria": "ficcion",
-        "image": "./img/perfectosmentirosos.jpg",
+        "image": "../img/perfectosmentirosos.jpg",
         "price": 2399,
         "stock": 7,
         "cantidad": 1
@@ -111,7 +111,7 @@ const libros = [
         "id": "13",
         "title": "Un cuento perfecto",
         "categoria": "ficcion",
-        "image": "./img/uncuentoperfecto.jpg",
+        "image": "../img/uncuentoperfecto.jpg",
         "price": 2199,
         "stock": 7,
         "cantidad": 1
@@ -120,7 +120,7 @@ const libros = [
         "id": "14",
         "title": "Heartstopper",
         "categoria": "infantil",
-        "image": "./img/heartstopper.jpg",
+        "image": "../img/heartstopper.jpg",
         "price": 3733,
         "stock": 7,
         "cantidad": 1
@@ -129,7 +129,7 @@ const libros = [
         "id": "15",
         "title": "Heartstopper 2",
         "categoria": "infantil",
-        "image": "./img/HeartsTopper2.jpg",
+        "image": "../img/HeartsTopper2.jpg",
         "price": 3440,
         "stock": 7,
         "cantidad": 1
@@ -138,7 +138,7 @@ const libros = [
         "id": "16",
         "title": "Fleur",
         "categoria": "ficcion",
-        "image": "./img/fleur.jpg",
+        "image": "../img/fleur.jpg",
         "price": 2520,
         "stock": 7,
         "cantidad": 1
@@ -147,7 +147,7 @@ const libros = [
         "id": "17",
         "title": "Antes de diciembre",
         "categoria": "ficcion",
-        "image": "./img/antesdediciembre.png",
+        "image": "../img/antesdediciembre.png",
         "price": 3025,
         "stock": 7,
         "cantidad": 1
@@ -156,7 +156,7 @@ const libros = [
         "id": "19",
         "title": "Scott pilgrim lo consigue",
         "categoria": "comics",
-        "image": "./img/descarga (3).jpg",
+        "image": "../img/descarga (3).jpg",
         "price": 1899,
         "stock": 7,
         "cantidad": 1
@@ -165,7 +165,7 @@ const libros = [
         "id": "20",
         "title": "Iznogud ve las estrellas",
         "categoria": "comics",
-        "image": "./img/descarga (2).jpg",
+        "image": "../img/descarga (2).jpg",
         "price": 2195,
         "stock": 7,
         "cantidad": 1
@@ -174,7 +174,7 @@ const libros = [
         "id": "21",
         "title": "Naftalina",
         "categoria": "comics",
-        "image": "./img/D_NQ_NP_679287-MLA44058395465_112020-O.jpg",
+        "image": "../img/D_NQ_NP_679287-MLA44058395465_112020-O.jpg",
         "price": 1999,
         "stock": 7,
         "cantidad": 1
@@ -183,7 +183,7 @@ const libros = [
         "id": "22",
         "title": "Las edades de la rata",
         "categoria": "comics",
-        "image": "./img/9788416131501.png",
+        "image": "../img/9788416131501.png",
         "price": 3389,
         "stock": 7,
         "cantidad": 1
@@ -192,7 +192,7 @@ const libros = [
         "id": "23",
         "title": "Esta mierda me supera",
         "categoria": "comics",
-        "image": "./img/4447.jpg",
+        "image": "../img/4447.jpg",
         "price": 2049,
         "stock": 7,
         "cantidad": 1
@@ -201,7 +201,7 @@ const libros = [
         "id": "24",
         "title": "Tomar refugio",
         "categoria": "comics",
-        "image": "./img/81+30Q-E7sL.jpg",
+        "image": "../img/81+30Q-E7sL.jpg",
         "price": 4919,
         "stock": 7,
         "cantidad": 1
@@ -210,7 +210,7 @@ const libros = [
         "id": "25",
         "title": "Ideas, papel y lapiz",
         "categoria": "ficcion",
-        "image": "./img/images.jpg",
+        "image": "../img/images.jpg",
         "price": 1846,
         "stock": 7,
         "cantidad": 1
@@ -219,7 +219,7 @@ const libros = [
         "id": "26",
         "title": "El amor esa gran encrusijada",
         "categoria": "ficcion",
-        "image": "./img/descarga.jpg",
+        "image": "../img/descarga.jpg",
         "price": 2035,
         "stock": 7,
         "cantidad": 1
@@ -228,7 +228,7 @@ const libros = [
         "id": "27",
         "title": "Hablame de ti",
         "categoria": "ficcion",
-        "image": "./img/descarga (1).jpg",
+        "image": "../img/descarga (1).jpg",
         "price": 1430,
         "stock": 7,
         "cantidad": 1
@@ -237,7 +237,7 @@ const libros = [
         "id": "28",
         "title": "Como la vida misma",
         "categoria": "ficcion",
-        "image": "./img/ComoLaVidaMisma_Página_01.jpg",
+        "image": "../img/ComoLaVidaMisma_Página_01.jpg",
         "price": 2158,
         "stock": 7,
         "cantidad": 1
@@ -246,7 +246,7 @@ const libros = [
         "id": "29",
         "title": "El dia menos pensado",
         "categoria": "ficcion",
-        "image": "./img/9789877389135.png",
+        "image": "../img/9789877389135.png",
         "price": 2499,
         "stock": 7,
         "cantidad": 1
@@ -255,7 +255,7 @@ const libros = [
         "id": "30",
         "title": "El viajero galactico",
         "categoria": "ficcion",
-        "image": "./img/41z2Sel3BkL.jpg",
+        "image": "../img/41z2Sel3BkL.jpg",
         "price": 4303,
         "stock": 7,
         "cantidad": 1
@@ -264,7 +264,7 @@ const libros = [
         "id": "31",
         "title": "Los ojos del che",
         "categoria": "historia",
-        "image": "./img/descarga (11).jpg",
+        "image": "../img/descarga (11).jpg",
         "price": 3819,
         "stock": 7,
         "cantidad": 1
@@ -273,7 +273,7 @@ const libros = [
         "id": "32",
         "title": "Que hubiera pasado si..?",
         "categoria": "historia",
-        "image": "./img/descarga (10).jpg",
+        "image": "../img/descarga (10).jpg",
         "price": 4009,
         "stock": 7,
         "cantidad": 1
@@ -282,7 +282,7 @@ const libros = [
         "id": "33",
         "title": "Masacre en el comedor",
         "categoria": "historia",
-        "image": "./img/descarga (9).jpg",
+        "image": "../img/descarga (9).jpg",
         "price": 2849,
         "stock": 7,
         "cantidad": 1
@@ -291,7 +291,7 @@ const libros = [
         "id": "34",
         "title": "Malvinas identidad de heroes",
         "categoria": "historia",
-        "image": "./img/descarga (8).jpg",
+        "image": "../img/descarga (8).jpg",
         "price": 2699,
         "stock": 7,
         "cantidad": 1
@@ -300,7 +300,7 @@ const libros = [
         "id": "35",
         "title": "La comida en la histia argentina",
         "categoria": "historia",
-        "image": "./img/9789500756419.png",
+        "image": "../img/9789500756419.png",
         "price": 2849,
         "stock": 7,
         "cantidad": 1
@@ -309,7 +309,7 @@ const libros = [
         "id": "36",
         "title": "Un maldito derecho",
         "categoria": "historia",
-        "image": "./img/978987362034.jpg",
+        "image": "../img/978987362034.jpg",
         "price": 1800,
         "stock": 7,
         "cantidad": 1
@@ -318,7 +318,7 @@ const libros = [
         "id": "37",
         "title": "52 juegos divertidos",
         "categoria": "infantil",
-        "image": "./img/9788868216603.png",
+        "image": "../img/9788868216603.png",
         "price": 985,
         "stock": 7,
         "cantidad": 1
@@ -327,7 +327,7 @@ const libros = [
         "id": "38",
         "title": "Destroza este diario",
         "categoria": "infantil",
-        "image": "./img/descarga (4).jpg",
+        "image": "../img/descarga (4).jpg",
         "price": 2570,
         "stock": 7,
         "cantidad": 1
@@ -336,7 +336,7 @@ const libros = [
         "id": "38",
         "title": "Red, la novela",
         "categoria": "infantil",
-        "image": "./img/descarga (5).jpg",
+        "image": "../img/descarga (5).jpg",
         "price": 2090,
         "stock": 7,
         "cantidad": 1
@@ -345,7 +345,7 @@ const libros = [
         "id": "40",
         "title": "Enciclopedia de animales",
         "categoria": "infantil",
-        "image": "./img/descarga (6).jpg",
+        "image": "../img/descarga (6).jpg",
         "price": 3095,
         "stock": 7,
         "cantidad": 1
@@ -354,7 +354,7 @@ const libros = [
         "id": "41",
         "title": "Jardineria y huertos familiares",
         "categoria": "infantil",
-        "image": "./img/descarga (7).jpg",
+        "image": "../img/descarga (7).jpg",
         "price": 3095,
         "stock": 7,
         "cantidad": 1
@@ -363,7 +363,7 @@ const libros = [
         "id": "42",
         "title": "Los mundos de caroline",
         "categoria": "infantil",
-        "image": "./img/Los_mundos_de_Coraline-920132946-large.jpg",
+        "image": "../img/Los_mundos_de_Coraline-920132946-large.jpg",
         "price": 2049,
         "stock": 7,
         "cantidad": 1
